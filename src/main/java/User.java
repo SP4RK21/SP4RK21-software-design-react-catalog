8685dfa1fe68bc121b4d064bfff03e4764fcdd1e
Sergei Tynianov <s.tynyanov@corp.vk.com>
2021-03-07 11:49:36 +0300

    create user model

---
+++ src/main/java/User.java
@@ -0,0 +1,34 @@
+import org.bson.Document;
+
+public class User {
+    public final int id;
+    public final String login;
+    public final Currency currency;
+
+
+    public User(Document doc) {
+        this(doc.getDouble("id").intValue(), doc.getString("login"), Currency.valueOf(doc.getString("currency")));
+    }
+
+    public User(int id, String login, Currency currency) {
+        this.id = id;
+        this.login = login;
+        this.currency = currency;
+    }
+
+    public Document toDocument() {
+        return new Document()
+                .append("id", id)
+                .append("login", login)
+                .append("currency", currency.name());
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", currency='" + currency.name() + '\'' +
+                '}';
+    }
+}
