7081a797fc2b6b8b467ac995301a2d506a604792
Sergei Tynianov <s.tynyanov@corp.vk.com>
2021-03-07 11:49:57 +0300

    create product model

---
+++ src/main/java/Product.java
@@ -0,0 +1,45 @@
+import org.bson.Document;
+
+public class Product {
+    public final int id;
+    public final String name;
+    public final Currency currency;
+    public final double price;
+
+
+    public Product(Document doc) {
+        this(doc.getDouble("id").intValue(),
+                doc.getString("login"),
+                Currency.valueOf(doc.getString("currency")),
+                doc.getDouble("price"));
+    }
+
+    public Product(int id, String name, Currency currency, double price) {
+        this.id = id;
+        this.name = name;
+        this.currency = currency;
+        this.price = price;
+    }
+
+    public Document toDocument() {
+        return new Document()
+                .append("id", id)
+                .append("name", name)
+                .append("currency", currency.name())
+                .append("price", price);
+    }
+
+    public Product convertedToCurrency(Currency currencyToConvert) {
+        return new Product(id, name, currencyToConvert, this.currency.convertPriceToCurrency(price, currencyToConvert));
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", currency='" + currency.name() + '\'' +
+                ", price='" + price + '\'' +
+                '}';
+    }
+}
